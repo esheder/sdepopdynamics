6a69c80cb61a1497a747ee7a24cb981ef35a7600
Eshed <esheder@gmail.com>
2023-12-03 15:17:40 +0200

    Fixed naming switch in outputs

--- scripts/sample_system_branching.sh
+++ scripts/sample_system_branching.sh
@@ -5,4 +5,4 @@ args=("$@")
 times="${args[*]:3}"
 seed=$((SLURM_ARRAY_TASK_ID + shift))
 
-popfeedback -s "$seed" -n "$2" branching --path "$3" --times "$times" >result_sde_"$seed".out
+popfeedback -s "$seed" -n "$2" branching --path "$3" --times "$times" >result_branching_"$seed".out
--- scripts/sample_system_sde.sh
+++ scripts/sample_system_sde.sh
@@ -5,4 +5,4 @@ args=("$@")
 times="${args[*]:4}"
 seed=$((SLURM_ARRAY_TASK_ID + shift))
 
-popfeedback -s "$seed" -n "$2" sde "$4" --path "$3" --times "$times" >result_branching_"$seed".out
+popfeedback -s "$seed" -n "$2" sde "$4" --path "$3" --times "$times" >result_sde_"$seed".out

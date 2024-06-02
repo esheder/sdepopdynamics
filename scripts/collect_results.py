26379a9efd416b5eac6423b7cb165d138cfbbbca
Eshed Magali <esheder@gmail.com>
2024-06-02 14:47:21 +0300

    Make it easier to run multiple seeds on one program call

--- scripts/collect_results.py
+++ scripts/collect_results.py
@@ -11,18 +11,45 @@ def parse_file(s: str) -> list[float]:
 def get_seed_off_fn(fn: str) -> int:
     return int(fn.split('_')[-1][:-4])
 
+def get_number_of_lines(fn: Path) -> int:
+    with fn.open('rb') as f:
+        return sum(1 for _ in f)
+
+def get_seeds(argseeds: list[int], fn: Path) -> list[int]:
+    if len(argseeds) != 2:
+        return argseeds
+    lines = get_number_of_lines(fn)
+    if lines == 2:
+        return argseeds
+    return list(range(*argseeds))
+
+def csv_format(s, t, v): 
+    return f"{s}, {t:.2f}, {v}"
+
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description="Tool to generate single csv file with data")
-    p.add_argument("d", type=Path, help="Directory where files are at")
+    p.add_argument("--directory", type=Path, default=None, help="Directory where files are at")
+    p.add_argument('--file', type=Path, help="File that includes lots of result lines")
     p.add_argument('o', type=Path, help="Output path")
     p.add_argument("t", type=float, nargs="+", help="Times list")
+    p.add_argument("--seeds", required=True, type=int, nargs="+", 
+            help="Seeds. If 2 are given, assumes a list between the two, if applicable.")
     args = p.parse_args()
     with args.o.open('w') as of:
         of.write("seed,time,population\n")
-        for fn in map(lambda x: args.d / x, listdir(args.d)):
-            with fn.open('r') as f:
-                vec = parse_file(f.read())
-            seed = get_seed_off_fn(str(fn))
-            for t, v in zip(args.t, vec):
-                of.write(f"{seed}, {t:.2f}, {v}\n")
+        if args.directory:
+            for fn in map(lambda x: args.d / x, listdir(args.d)):
+                with fn.open('r') as f:
+                    vec = parse_file(f.read())
+                seed = get_seed_off_fn(str(fn))
+                of.write('\n'.join(csv_format(seed, t, v) for t, v in zip(args.t, vec)))
+        if args.file:
+            seeds = get_seeds(args.seeds, args.file)
+            with args.file.open('r') as f:
+                vecs = (parse_file(line) for line in f)
+                of.write("\n".join(csv_format(seed, t, v) 
+                    for seed, vec in zip(seeds, vecs) 
+                    for t, v in zip(args.t, vec)))
+
+
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ struct Cli {
     model: Model,
 
     /// Random number generator seed
-    #[arg(short, long, value_name = "Seed", default_value = "48")]
-    seed: u64,
+    #[arg(short, long, num_args = 1.., value_delimiter = ' ', value_name = "Seed", default_values = ["48"])]
+    seeds: Vec<u64>,
 
     /// Population size
     #[arg(short, long, value_name = "PopSize")]
@@ -21,6 +21,10 @@ struct Cli {
     /// Verbose flag
     #[arg(short, long)]
     v: bool,
+
+    /// Debug flag
+    #[arg(short, long)]
+    debug: bool,
 }
 
 #[derive(Debug, Subcommand)]
@@ -66,46 +70,54 @@ struct Params {
 
 fn main() {
     let args = Cli::parse();
-    let mut rng = ChaCha20Rng::seed_from_u64(args.seed);
-
-    let pops = match &args.model {
-        Model::Branching { ex } => {
-            let par = &ex.params;
-            let params = if let Some(path) = par.path.as_ref() {
-                Parameters::from_json_file(path)
-            } else {
-                let v: Vec<f64> = par.values.as_deref().unwrap().to_vec();
-                Parameters::from_vec(&v)
-            };
-            if args.v {
-                println!("{:?}", &params);
-                println!("Times: {:?}", &ex.times);
-                println!("Seed: {}", args.seed);
-                println!("Initial population: {}", args.n);
-            }
-            let fun = |x, y| popfeedback::sample_branching_at_time(&params, x, y, &mut rng);
-            popfeedback::sample_at_times(args.n, &ex.times, fun)
-        }
-        Model::Sde { dt, ex } => {
-            let par = &ex.params;
-            let params = if let Some(path) = par.path.as_ref() {
-                Parameters::from_json_file(path)
-            } else {
-                let v: Vec<f64> = par.values.as_deref().unwrap().to_vec();
-                Parameters::from_vec(&v)
-            };
-            if args.v {
-                println!("{:?}", &params);
-                println!("Times: {:?}", &ex.times);
-                println!("Seed: {}", args.seed);
-                println!("Initial population: {}", args.n);
-            }
-            let fun = |x: f64, y| popfeedback::sample_sde_at_time(&params, x, y, *dt, &mut rng);
-            popfeedback::sample_at_times(args.n, &ex.times, fun)
-        }
+    let seeds = match args.seeds.len() {
+        2 => (args.seeds[0]..args.seeds[1]).collect(),
+        _ => args.seeds,
     };
 
-    println!("{:?}", pops);
+    let (dt, ex) = match &args.model {
+        Model::Sde { dt, ex } => (dt, ex),
+        Model::Branching { ex } => (&0., ex),
+    };
+
+    let par = &ex.params;
+    let params = if let Some(path) = par.path.as_ref() {
+        Parameters::from_json_file(path)
+    } else {
+        let v: Vec<f64> = par.values.as_deref().unwrap().to_vec();
+        Parameters::from_vec(&v)
+    };
+
+    if args.v {
+        println!("{:?}", &params);
+        println!("Seeds: {:?}", seeds);
+        println!("Initial population: {}", args.n);
+        println!("Times: {:?}", &ex.times);
+        match &args.model {
+            Model::Sde { .. } => println!("Time step: {}", dt),
+            Model::Branching { .. } => (),
+        }
+    }
+
+    if args.debug {
+        return;
+    }
+
+    for seed in &seeds {
+        let mut rng = ChaCha20Rng::seed_from_u64(*seed);
+
+        let pops = match &args.model {
+            Model::Branching { .. } => {
+                let fun = |x, y| popfeedback::sample_branching_at_time(&params, x, y, &mut rng);
+                popfeedback::sample_at_times(args.n, &ex.times, fun)
+            }
+            Model::Sde { .. } => {
+                let fun = |x: f64, y| popfeedback::sample_sde_at_time(&params, x, y, *dt, &mut rng);
+                popfeedback::sample_at_times(args.n, &ex.times, fun)
+            }
+        };
+        println!("{:?}", pops);
+    }
 }
 
 #[test]

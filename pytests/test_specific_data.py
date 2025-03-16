04929260f8ae86b9ec400e915fa877599e3df3a4
sharonkb7 <sharonkb7@gmail.com>
2025-03-16 09:11:39 +0200

    Added a test that the populations grow according to Kolmogorov
    It passes. Then why are the plots saying otherwise?

--- pytests/test_specific_data.py
+++ pytests/test_specific_data.py
@@ -7,10 +7,12 @@ from pathlib import Path
 import numpy as np
 from biopop_closure.kolmogorov import kolmogorov_multiplicity
 from biopop_closure.moment_closures import moment
+from itertools import product
+import pytest
 
-cpath = Path(__file__).parent.parent / 'runs' / 'european_badgers' / 'multiple_birth_high_immigration.json'
-cpath = cpath.parent / 'high_immigration.json'
-
+cpath = Path(__file__).parent.parent / 'runs'
+fnames = [f"{s}_immigration{'_multiple_birth' if multi else ''}.json" for s, multi in product(('high', 'low'), (True, False))]
+dnames = [cpath / f"european_{animal}" for animal in ("badgers", "foxes")]
 
 def _load_data(path):
     with path.open('r') as f:
@@ -21,10 +23,11 @@ def _key_replacement(d, dr):
     return {dr[key] if key in dr else key: val for key, val in d.items()}
 
 
-def test_kolmogorov_for_badgers_rises():
-    data = _key_replacement(_load_data(cpath), {'I': 'i', 'multiplicity': 'm'})
+@pytest.mark.parametrize("path", [dname / fname for dname, fname in product(dnames, fnames)])
+def test_kolmogorov_for_badgers_rises(path):
+    data = _key_replacement(_load_data(path), {'I': 'i', 'multiplicity': 'm'})
     pop = kolmogorov_multiplicity(**data,
-                                  t=np.linspace(0, 10, 5),
+                                  t=np.linspace(0, 20, 20),
                                   p0=5,
                                   nmax=300
                                   )

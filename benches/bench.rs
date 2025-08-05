2cd6de636888421e4e33327e5e25c7d40481d0fd
Eshed Magali <esheder@gmail.com>
2025-08-05 11:50:34 +0300

    Added high population benchmark to show that things becomes worse for high population counts

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,7 +7,7 @@ use rand::rng;
 use test::{black_box, Bencher};
 
 #[bench]
-fn bench_sde(b: &mut Bencher) {
+fn bench_sde_lowpop(b: &mut Bencher) {
     let p = Parameters {
         a1: 10.,
         a2: 30.,
@@ -21,13 +21,41 @@ fn bench_sde(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_branching(b: &mut Bencher) {
+fn bench_branching_lowpop(b: &mut Bencher) {
     let p = Parameters {
         a1: 10.,
         a2: 30.,
         b1: 0.,
         b2: 0.,
         I: 3000.,
+        multiplicity: vec![0.5, 0.5],
+    };
+    let mut rng = rng();
+    b.iter(|| black_box(sample_branching_at_time(&p, 4000, 20., &mut rng)));
+}
+
+#[bench]
+fn bench_sde_highpop(b: &mut Bencher) {
+    let p = Parameters {
+        a1: 60.,
+        a2: 30.,
+        b1: 0.01,
+        b2: 0.,
+        I: 5.,
+        multiplicity: vec![0.5, 0.5],
+    };
+    let mut rng = rng();
+    b.iter(|| black_box(sample_sde_at_time(&p, 4000, 20., 1e-4, &mut rng)));
+}
+
+#[bench]
+fn bench_branching_highpop(b: &mut Bencher) {
+    let p = Parameters {
+        a1: 60.,
+        a2: 30.,
+        b1: 0.01,
+        b2: 0.,
+        I: 5.,
         multiplicity: vec![0.5, 0.5],
     };
     let mut rng = rng();

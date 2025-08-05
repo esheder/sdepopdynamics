22ba20722641bedd89d518b7baf8440078f42513
Eshed Magali <esheder@gmail.com>
2025-08-05 17:23:03 +0300

    Changed benchmark to fit the paper case

--- benches/bench.rs
+++ benches/bench.rs
@@ -37,27 +37,27 @@ fn bench_branching_lowpop(b: &mut Bencher) {
 #[bench]
 fn bench_sde_highpop(b: &mut Bencher) {
     let p = Parameters {
-        a1: 60.,
-        a2: 30.,
-        b1: 0.01,
+        a1: 0.4,
+        a2: 0.15,
+        b1: 0.0001,
         b2: 0.,
-        I: 5.,
-        multiplicity: vec![0.5, 0.5],
+        I: 2.,
+        multiplicity: vec![0.11, 0.51, 0.28, 0.08, 0.02],
     };
     let mut rng = rng();
-    b.iter(|| black_box(sample_sde_at_time(&p, 4000, 20., 1e-4, &mut rng)));
+    b.iter(|| black_box(sample_sde_at_time(&p, 3750, 20., 1e-2, &mut rng)));
 }
 
 #[bench]
 fn bench_branching_highpop(b: &mut Bencher) {
     let p = Parameters {
-        a1: 60.,
-        a2: 30.,
-        b1: 0.01,
+        a1: 0.4,
+        a2: 0.15,
+        b1: 0.0001,
         b2: 0.,
-        I: 5.,
-        multiplicity: vec![0.5, 0.5],
+        I: 2.,
+        multiplicity: vec![0.11, 0.51, 0.28, 0.08, 0.02],
     };
     let mut rng = rng();
-    b.iter(|| black_box(sample_branching_at_time(&p, 4000, 20., &mut rng)));
+    b.iter(|| black_box(sample_branching_at_time(&p, 3750, 20., &mut rng)));
 }
